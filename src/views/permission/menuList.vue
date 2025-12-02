76436d3693a627ff9ce5cd43c55aa00444371652
liuml <723164417@qq.com>
2025-12-02 10:55:48 +0800

    fix: 修复菜单编辑页面标签结构错误
    - 修复 el-col 标签不匹配导致的 Vue 编译错误
    - 恢复是否外链开关的 disabled 属性为条件判断

--- src/views/permission/menuList.vue
+++ src/views/permission/menuList.vue
@@ -251,10 +251,10 @@
                     </el-col>
                     <el-col :span="3">
                         <el-form-item prop="is_external_links" label="是否外链">
-                            <el-switch v-model="formData.is_external_links" inline-prompt active-text="是" inactive-text="否" :active-value="1" :inactive-value="0" disabled />
+                            <el-switch v-model="formData.is_external_links" inline-prompt active-text="是" inactive-text="否" :active-value="1" :inactive-value="0" :disabled="formData.type === MENU_TYPE.BUTTON" />
                         </el-form-item>
                     </el-col>
-                    <el-col :span="4"></el-col>
+                    <el-col :span="4">
                         <el-form-item prop="is_new_window" label="新窗口打开" v-show="formData.is_external_links === 1">
                             <template #label>
                                 <span class="xl-label-with-icon">
